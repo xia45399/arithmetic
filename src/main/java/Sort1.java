36155f1ad3e7670a091e707cd0f773708bd9dcad
xia45399 <453993887@qq.com>
2018-08-22 21:53:20 +0800

    init

---
+++ src/main/java/Sort1.java
@@ -0,0 +1,28 @@
+/**
+ * 1冒泡排序
+ */
+public class Sort1 {
+    public static void main(String[] args) {
+        int[] arr = {34, 223, 52, 7, 256, 1, 67, 8, 123, 200, 65, 8, 93, 12, 4645, 7, 568, 56};
+        sort(arr);
+    }
+    private static void sort(int arr[]){
+        for (int end = arr.length - 1; end > 0; end--) {
+            for (int i = 0; i < end; i++) {
+                if (arr[i] < arr[i + 1]) {
+                    int tem = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = tem;
+                }
+            }
+            print(arr);
+        }
+    }
+
+    private static void print(int[] arr) {
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+}
