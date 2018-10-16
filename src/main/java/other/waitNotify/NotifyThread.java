5809c46d5b5e2633b9037f6c866837df776841f6
xia45399 <453993887@qq.com>
2018-10-16 16:05:29 +0800

    wait notifyAll

---
+++ src/main/java/other/waitNotify/NotifyThread.java
@@ -0,0 +1,30 @@
+package other.waitNotify;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class NotifyThread extends Thread {
+    private List list;
+
+
+    public NotifyThread(List list) {
+        this.list = list;
+    }
+
+    @Override
+    public void run() {
+        int i = 1;
+        while (true) {
+            synchronized (list) {
+                list.add("第" + (i++) + "条消息");
+                list.notifyAll();
+
+            }
+            try {
+                TimeUnit.SECONDS.sleep(5);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
